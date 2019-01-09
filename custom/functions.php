5f8d1d7986a5fcba5f5333b07d36a0ff2b2d77db
leroy <leroy@lorey.nl>
2019-01-09 21:22:41 +0100

    Set default shipment address state to false

--- custom/functions.php
+++ custom/functions.php
@@ -15,9 +15,21 @@ if ( ! defined( 'ABSPATH' ) ) {
  * functions.php
  * Add PHP snippets here
  */
+
+/**
+ * Add fontawesome
+ */
 add_action( 'wp_enqueue_scripts', function () {
     wp_enqueue_style( 'fontawesome', 'https://use.fontawesome.com/releases/v5.6.3/css/all.css' );
 });
 
+/**
+ * Set default shipping address state to false
+ */
+add_filter( 'woocommerce_ship_to_different_address_checked', '__return_false' );
+
+/**
+ * Remove checkout button
+ */
 remove_action( 'woocommerce_proceed_to_checkout', 'action_woocommerce_proceed_to_checkout', 10, 2 );
 
